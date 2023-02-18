8bb2217c57865ead067f1b50a46771b8c1bb2b2b
Khalyutkin Victor <53880888+KhalyutkinVictor@users.noreply.github.com>
2023-02-18 12:28:56 +0300

    Delete params-local.php

--- config/params-local.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'yandex' => [
-        'Iam' => 't1.9euelZqek4-SnYmSj5yNjcqanZmJiu3rnpWam8qYi42WzMyamJGQnpySyMzl9PcOKz5g-e98V1OP3fT3Tlk7YPnvfFdTjw.1lNjU_ZYLu60f7LDJ_Agp1nJNgdDF0ZIP_FeWCjK9zAU5ECAZLQpUP-WOVBOgyNKiwKhheKv766HyWRzgpmrDw',
-        'folderId' => 'b1g4mu9r8j2spdpkn2ao'
-    ]
-];
