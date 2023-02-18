de221b7bd6ee31ee47f6ae55301d08fd8c67d630
Khalyutkin Victor <53880888+KhalyutkinVictor@users.noreply.github.com>
2023-02-18 13:37:25 +0300

    Update ChatController.php

--- controllers/ChatController.php
+++ controllers/ChatController.php
@@ -8,4 +8,26 @@ use yii\web\Controller;
 class ChatController extends Controller
 {
     use QaTrait;
+    
+    public function behaviors()
+    {
+        return array_merge(parent::behaviors(), [
+            'corsFilter'  => [
+                'class' => \yii\filters\Cors::className(),
+                'cors'  => [
+                    // restrict access to domains:
+                    'Origin'                           => ['*'],
+                    'Access-Control-Request-Method'    => ['POST', 'GET'],
+                    'Access-Control-Allow-Credentials' => true,
+                    'Access-Control-Max-Age'           => 3600,                 // Cache (seconds)
+                ],
+            ],
+        ]);
+    }
+    
+    public function beforeAction($action)
+    {
+        $this->enableCsrfValidation = false;
+        return parent::beforeAction($action);
+    }
 }
