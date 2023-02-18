c5e15bc0c621a2bbcbd1297b252c6011b88e89ed
Khalyutkin Victor <53880888+KhalyutkinVictor@users.noreply.github.com>
2023-02-18 12:25:36 +0300

    Update Stt.php

--- models/stt/Stt.php
+++ models/stt/Stt.php
@@ -2,17 +2,47 @@
 
 namespace app\models\stt;
 
+use GuzzleHttp\Client;
 use yii\base\Model;
+use yii\helpers\Json;
+use yii\web\UploadedFile;
 
 class Stt extends Model
 {
     
     const API_URL = 'https://stt.api.cloud.yandex.net/speech/v1/stt:recognize?lang=ru-RU';
     
+    /**
+     * @param UploadedFile $file
+     * @return false|string
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
     public function recognize($file)
     {
         $url = self::API_URL . '&folderId=' . \Yii::$app->params['yandex']['folderId'];
         $authToken = \Yii::$app->params['yandex']['Iam'];
+        $fileContent = file_get_contents($file->tempName);
+        if ($fileContent === false) {
+            return false;
+        }
+        
+        $httpClient = new Client();
+        $res = null;
+        try {
+            $res = $httpClient->post($url, [
+                'headers' => [ 'Authorization' => 'Bearer ' . $authToken],
+                'body' => $fileContent
+            ]);
+            $res = Json::decode($res->getBody());
+        } catch (\Throwable $e) {
+            return false;
+        }
+        
+        if (!$res || !array_key_exists('result', $res)) {
+            return false;
+        }
+        
+        return $res['result'];
     }
     
 }
